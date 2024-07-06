09d6b34eeb3f9dad9debbd670a5449c480a1508a
Enrico Degregori <degregori@dkrz.de>
2024-07-07 00:50:43 +0200

    kernel: unroll last warp

--- src/reduction1dvector.cu
+++ src/reduction1dvector.cu
@@ -5,7 +5,39 @@
 
 using namespace std::chrono;
 
+__device__ void warpReduce(volatile int* sdata, unsigned int tid) {
+	sdata[tid] += sdata[tid + 32];
+	sdata[tid] += sdata[tid + 16];
+	sdata[tid] += sdata[tid + 8];
+	sdata[tid] += sdata[tid + 4];
+	sdata[tid] += sdata[tid + 2];
+	sdata[tid] += sdata[tid + 1];
+}
+
 __global__ void reduce1dKernel(int *g_idata, int *g_odata) {
+
+	// use dynamic shared memory
+	extern __shared__ int sdata[];
+	// each thread loads one element from global to shared mem
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*(blockDim.x*2) + threadIdx.x;
+	sdata[tid] = g_idata[i] + g_idata[i+blockDim.x];
+	__syncthreads();
+	// do reduction in shared mem
+	for (unsigned int s=blockDim.x/2; s>32; s>>=1) {
+		if (tid < s) {
+			sdata[tid] += sdata[tid + s];
+		}
+		__syncthreads();
+	}
+
+	if (tid < 32) warpReduce(sdata, tid);
+
+	// write result for this block to global mem
+	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
+__global__ void reduce1dKernelFlexible(int *g_idata, int *g_odata) {
 
 	// use dynamic shared memory
 	extern __shared__ int sdata[];
@@ -21,6 +53,7 @@ __global__ void reduce1dKernel(int *g_idata, int *g_odata) {
 		}
 		__syncthreads();
 	}
+
 	// write result for this block to global mem
 	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
 }
@@ -37,7 +70,7 @@ void reduce1d(int *g_idata, int *g_odata, int size) {
 	if (blocksPerGrid == 1) {
 
 		auto start = high_resolution_clock::now();
-		reduce1dKernel<<<blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int)>>>(g_idata, g_odata);
+		reduce1dKernelFlexible<<<blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int)>>>(g_idata, g_odata);
 		cudaError_t err = cudaDeviceSynchronize();
 		if ( err != cudaSuccess ) {
 			std::cout << "CUDA error: " << cudaGetErrorString(err) << std::endl;
