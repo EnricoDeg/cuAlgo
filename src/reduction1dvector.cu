9468b2fb07184c36c43b0cd9b097ccd7621c9adf
Enrico Degregori <degregori@dkrz.de>
2024-07-07 11:56:13 +0200

    kernel: full unrolling

--- src/reduction1dvector.cu
+++ src/reduction1dvector.cu
@@ -5,15 +5,17 @@
 
 using namespace std::chrono;
 
-__device__ void warpReduce(volatile int* sdata, unsigned int tid) {
-	sdata[tid] += sdata[tid + 32];
-	sdata[tid] += sdata[tid + 16];
-	sdata[tid] += sdata[tid + 8];
-	sdata[tid] += sdata[tid + 4];
-	sdata[tid] += sdata[tid + 2];
-	sdata[tid] += sdata[tid + 1];
+template <unsigned int blockSize>
+__device__ void warpReduce(volatile int* sdata, int tid) {
+if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
+if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
+if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
+if (blockSize >= 8) sdata[tid] += sdata[tid + 4];
+if (blockSize >= 4) sdata[tid] += sdata[tid + 2];
+if (blockSize >= 2) sdata[tid] += sdata[tid + 1];
 }
 
+template <unsigned int blockSize>
 __global__ void reduce1dKernel(int *g_idata, int *g_odata) {
 
 	// use dynamic shared memory
@@ -24,14 +26,32 @@ __global__ void reduce1dKernel(int *g_idata, int *g_odata) {
 	sdata[tid] = g_idata[i] + g_idata[i+blockDim.x];
 	__syncthreads();
 	// do reduction in shared mem
-	for (unsigned int s=blockDim.x/2; s>32; s>>=1) {
-		if (tid < s) {
-			sdata[tid] += sdata[tid + s];
+	if (blockSize >= 1024) {
+		if (tid < 512) {
+			sdata[tid] += sdata[tid + 512];
 		}
 		__syncthreads();
 	}
-
-	if (tid < 32) warpReduce(sdata, tid);
+	if (blockSize >= 512) {
+		if (tid < 256) {
+			sdata[tid] += sdata[tid + 256];
+		}
+		__syncthreads();
+	}
+	if (blockSize >= 256) {
+		if (tid < 128) {
+			sdata[tid] += sdata[tid + 128];
+		}
+		__syncthreads();
+	}
+	if (blockSize >= 128) {
+		if (tid < 64) {
+			sdata[tid] += sdata[tid + 64];
+		}
+		__syncthreads();
+	}
+	
+	if (tid < 32) warpReduce<blockSize>(sdata, tid);
 
 	// write result for this block to global mem
 	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
@@ -88,7 +108,43 @@ void reduce1d(int *g_idata, int *g_odata, int size) {
 			exit(EXIT_FAILURE);
 		}
 		auto start = high_resolution_clock::now();
-		reduce1dKernel<<<blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int)>>>(g_idata, d_buffer);
+		switch (threadsPerBlock) {
+			case 1024:
+			reduce1dKernel<1024><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 512:
+			reduce1dKernel<512><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 256:
+			reduce1dKernel<256><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 128:
+			reduce1dKernel<128><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 64:
+			reduce1dKernel< 64><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 32:
+			reduce1dKernel< 32><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 16:
+			reduce1dKernel< 16><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 8:
+			reduce1dKernel< 8><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 4:
+			reduce1dKernel< 4><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 2:
+			reduce1dKernel< 2><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+			case 1:
+			reduce1dKernel< 1><<< blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int) >>>(g_idata, d_buffer);
+			break;
+		}
+
+		// reduce1dKernel<<<blocksPerGrid3, threadsPerBlock3, (size_t)threadsPerBlock*sizeof(int)>>>(g_idata, d_buffer);
 		err = cudaDeviceSynchronize();
 		if ( err != cudaSuccess ) {
 			std::cout << "CUDA error: " << cudaGetErrorString(err) << std::endl;
