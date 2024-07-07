bf971573497818d65d6506e303644a8da488a4ab
Enrico Degregori <degregori@dkrz.de>
2024-07-07 19:18:18 +0200

    kernel: use shared memory for transposition

--- src/transposeMatrix.cu
+++ src/transposeMatrix.cu
@@ -4,27 +4,41 @@
 #include "cuAlgo.hpp"
 #include <chrono>
 
+using namespace std::chrono;
+
 #define TILE_DIM 32
 #define BLOCK_ROWS (TILE_DIM / 4)
 
 __global__ void transposeMatrixKernel(float *idata, float *odata)
 {
+	__shared__ float tile[TILE_DIM][TILE_DIM];
+
 	int x = blockIdx.x * TILE_DIM + threadIdx.x;
 	int y = blockIdx.y * TILE_DIM + threadIdx.y;
 	int width = gridDim.x * TILE_DIM;
 
-	for (int j = 0; j < TILE_DIM; j+= BLOCK_ROWS)
-		odata[x*width + (y+j)] = idata[(y+j)*width + x];
+	for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
+		tile[threadIdx.y+j][threadIdx.x] = idata[(y+j)*width + x];
+
+	__syncthreads();
+
+	x = blockIdx.y * TILE_DIM + threadIdx.x;  // transpose block offset
+	y = blockIdx.x * TILE_DIM + threadIdx.y;
+
+	for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
+		odata[(y+j)*width + x] = tile[threadIdx.x][threadIdx.y + j];
+
 }
 
 void transposeMatrix(float *idata, float *odata, unsigned int size_x, unsigned int size_y) {
 
 	dim3 blocksPerGrid3(size_x / TILE_DIM, size_y / TILE_DIM, 1);
 	dim3 threadsPerBlock3(TILE_DIM, BLOCK_ROWS, 1);
+
+	auto start = high_resolution_clock::now();
 	transposeMatrixKernel<<< blocksPerGrid3, threadsPerBlock3 >>>(idata, odata);
-	cudaError_t err = cudaDeviceSynchronize();
-	if ( err != cudaSuccess ) {
-		std::cout << "CUDA error: " << cudaGetErrorString(err) << std::endl;
-		exit(EXIT_FAILURE);
-	}
+	check_cuda( cudaDeviceSynchronize() );
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
 }
