0002769ba247b95a2d787d61ed49c512549c8967
Enrico Degregori <degregori@dkrz.de>
2024-07-16 22:23:21 +0200

    kernel: initial implementation of general matrix multiplication

--- src/cuAlgo.hpp
+++ src/cuAlgo.hpp
@@ -3,3 +3,6 @@ void check_cuda(cudaError_t error);
 void reduce1d(int *g_idata, int *g_odata, int size);
 
 void transposeMatrix(float *idata, float *odata, unsigned int size_x, unsigned int size_y);
+
+void gMatMul(int M, int N, int K, float alpha, const float *A,
+             const float *B, float beta, float *C);
---
+++ src/gMatMul.cu
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cuda.h>
+#include "cuAlgo.hpp"
+#include <chrono>
+
+using namespace std::chrono;
+
+__global__ void gMatMulKernel(int M, int N, int K, float alpha, const float *A,
+                              const float *B, float beta, float *C) {
+	// compute position in C that this thread is responsible for
+	const uint x = blockIdx.x * blockDim.x + threadIdx.x;
+	const uint y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	// `if` condition is necessary for when M or N aren't multiples of 32.
+	if (x < M && y < N) {
+		float tmp = 0.0;
+		for (int i = 0; i < K; ++i) {
+			tmp += A[x * K + i] * B[i * N + y];
+		}
+		// C = α*(A@B)+β*C
+		C[x * N + y] = alpha * tmp + beta * C[x * N + y];
+	}
+}
+
+int div_ceil(int numerator, int denominator)
+{
+		std::div_t res = std::div(numerator, denominator);
+		return res.rem ? (res.quot + 1) : res.quot;
+}
+
+void gMatMul(int M, int N, int K, float alpha, const float *A,
+             const float *B, float beta, float *C) {
+
+	// create as many blocks as necessary to map all of C
+	dim3 blocksPerGrid3(div_ceil(M, 32), div_ceil(N, 32), 1);
+	// 32 * 32 = 1024 thread per block
+	dim3 threadsPerBlock3(32, 32, 1);
+
+	std::cout << "threadsPerBlock = " << 32 << ", " << 32 << std::endl;
+	std::cout << "blocksPerGrid   = " << div_ceil(M, 32) << ", " << div_ceil(N, 32) << std::endl;
+
+	auto start = high_resolution_clock::now();
+	gMatMulKernel<<< blocksPerGrid3, threadsPerBlock3 >>>(M, N, K, alpha, A, B, beta, C);
+	check_cuda( cudaDeviceSynchronize() );
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+}
