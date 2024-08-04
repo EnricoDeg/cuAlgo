4e1d0fadddd04b5881ffbdd801704677956e4f78
Enrico Degregori <degregori@dkrz.de>
2024-08-04 16:10:26 +0200

    header: update library header

--- src/cuAlgo.hpp
+++ src/cuAlgo.hpp
@@ -1,8 +1,17 @@
+#ifndef CUALGO_H
+#define CUALGO_H
+
+#include <cuda.h>
+
 void check_cuda(cudaError_t error);
 
-void reduce1d(int *g_idata, int *g_odata, int size);
+void reduce1dVector(int *g_idata, int *g_odata, int size);
 
 void transposeMatrix(float *idata, float *odata, unsigned int size_x, unsigned int size_y);
 
 void gMatMul(int M, int N, int K, float alpha, const float *A,
              const float *B, float beta, float *C);
+
+void reduce1dMatrix(int * B, int * C, size_t N, size_t K);
+
+#endif
