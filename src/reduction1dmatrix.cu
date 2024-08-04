5a2fc5a9696776228b5bc27b208865430d9eabcb
Enrico Degregori <degregori@dkrz.de>
2024-08-04 15:47:01 +0200

    matrix1dreduction: generalize with 3 kernels

--- src/reduction1dmatrix.cu
+++ src/reduction1dmatrix.cu
@@ -6,10 +6,10 @@
 
 using namespace std::chrono;
 
-#define WARP_SIZE 32
-#define WARPS_PER_BLOCK 32
-#define THREADS_PER_BLOCK 1024  // WARP_SIZE * WARPS_PER_BLOCK
-#define COMPUTE_PER_THREAD 128
+#define THREADS_PER_BLOCK 1024
+#define THREADS_PER_BLOCK_X 32
+#define THREADS_PER_BLOCK_Y 32
+#define COMPUTE_PER_THREAD 64
 
 __global__ void reduction1dMatrixKernel(const int *__restrict__ B,
                                               int *__restrict__ C,
@@ -21,16 +21,37 @@ __global__ void reduction1dMatrixKernel(const int *__restrict__ B,
 	if (tid > N * chunks)
 		return;
 
-	const size_t col   = ( blockIdx.x * THREADS_PER_BLOCK + threadIdx.x ) % N;
-	const size_t chunk = ( blockIdx.x * THREADS_PER_BLOCK + threadIdx.x ) / N;
+	const size_t col   = tid % N;
+	const size_t chunk = tid / N;
+	const size_t tidm = col + chunk * N;
 
 	int tmp = 0;
-	int offset = chunk * ( N * K / chunks );
 #pragma unroll
-	for (size_t i = 0; i < K/chunks; ++i) {
-		tmp += B[i * N + col + offset];
+	for (size_t i = 0; i < K / chunks; ++i) {
+		tmp += B[i * N*chunks + tidm];
 	}
 	C[col+chunk*N] = tmp;
+}
+
+__global__ void reduction1dMatrixKernel1(const int *__restrict__ B,
+                                               int *__restrict__ C,
+                                         size_t                  N,
+                                         size_t                  K,
+                                         size_t             chunks) {
+
+	const size_t tidx = blockIdx.x * THREADS_PER_BLOCK_X + threadIdx.x;
+	const size_t tidy = blockIdx.y * THREADS_PER_BLOCK_Y + threadIdx.y;
+	if (tidx + N * tidy > N * chunks)
+		return;
+
+	const size_t tidm = tidx + tidy * N;
+
+	int tmp = 0;
+#pragma unroll
+	for (size_t i = 0; i < K / chunks; ++i) {
+		tmp += B[i * N*chunks + tidm];
+	}
+	C[tidx+tidy*N] = tmp;
 }
 
 void reduce1dMatrix(int *  B,
@@ -40,8 +61,32 @@ void reduce1dMatrix(int *  B,
 
 	size_t chunks = K / COMPUTE_PER_THREAD;
 	std::cout << "chunks = " << chunks << std::endl;
+	std::cout << "K = " << K << std::endl;
 
-	if (chunks > 1) {
+	if (chunks > THREADS_PER_BLOCK_Y) {
+
+		int * d_buffer;
+		check_cuda( cudaMalloc(&d_buffer, N * chunks *sizeof(int)) );
+
+		dim3 block(THREADS_PER_BLOCK_X, THREADS_PER_BLOCK_Y);
+		dim3 grid(div_ceil(N, THREADS_PER_BLOCK_X), div_ceil(chunks, THREADS_PER_BLOCK_Y));
+
+		std::cout << "threadsPerBlock = " << THREADS_PER_BLOCK_X << ", "
+		          << THREADS_PER_BLOCK_Y << std::endl;
+		std::cout << "blocksPerGrid   = " << div_ceil(N, THREADS_PER_BLOCK_X) << ", "
+		          << div_ceil(chunks, THREADS_PER_BLOCK_Y) << std::endl;
+
+		auto start = high_resolution_clock::now();
+		reduction1dMatrixKernel1<<<grid, block>>>(B, d_buffer, N, K, chunks);
+		check_cuda( cudaDeviceSynchronize() );
+		auto stop = high_resolution_clock::now();
+		auto duration = duration_cast<microseconds>(stop - start);
+		std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+
+		reduce1dMatrix(d_buffer, C, N, chunks);
+
+		check_cuda( cudaFree ( d_buffer ) );
+	} else if (chunks < THREADS_PER_BLOCK_Y && chunks > 1) {
 
 		int * d_buffer;
 		check_cuda( cudaMalloc(&d_buffer, N * chunks *sizeof(int)) );
