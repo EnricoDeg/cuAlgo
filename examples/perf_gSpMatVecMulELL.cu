d35ae1abd7fe95607adbf2531c15afca77cfde3d
Enrico Degregori <degregori@dkrz.de>
2024-11-10 13:19:58 +0100

    refactoring: naming of functions and files (gSpMatVecMulELL)

--- examples/perf_gSpMatVecMulELL.cu
+++ examples/perf_gSpMatVecMulELL.cu
@@ -1,5 +1,5 @@
 /*
- * @file generalSparseMatrixVectorMultiplicationELL.cu
+ * @file perf_gSpMatVecMulELL.cu
  *
  * @copyright Copyright (C) 2024 Enrico Degregori <enrico.degregori@gmail.com>
  *
@@ -43,11 +43,11 @@ int main(int argc, char *argv[])
 
 	// Allocate enough storage for the matix.  We allocate more than
 	// is needed in order to simplify the code
-	unsigned int * columns  = (unsigned int *)malloc(   nrows * nnz  * sizeof(unsigned int));
-	int          * values   = (         int *)malloc(   nrows * nnz  * sizeof(         int));
-	int          * x        = (         int *)malloc(   nrows        * sizeof(         int));
-	int          * y        = (         int *)malloc(   nrows        * sizeof(         int));
-	int          * solution = (         int *)malloc(   nrows        * sizeof(         int));
+	unsigned int * columns  = (unsigned int   *)malloc(   nrows * nnz  * sizeof(unsigned int  ));
+	float        * values   = (         float *)malloc(   nrows * nnz  * sizeof(         float));
+	float        * x        = (         float *)malloc(   nrows        * sizeof(         float));
+	float        * y        = (         float *)malloc(   nrows        * sizeof(         float));
+	float        * solution = (         float *)malloc(   nrows        * sizeof(         float));
 
 	// Create a sparse matrix with nnz non zeros per row constant.
 	// The non zero location and values are set randomly
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
 	// Perform a matrix-vector multiply: y = A*x
 	// Very inefficient implementation here
 	for (unsigned int i = 0; i < nrows; ++i) {
-		int sum = 0;
+		float sum = 0.0;
 		for (unsigned int j=0; j<nnz; ++j) {
 			unsigned int offset = j * nrows + i;
 			sum += values[offset] * x[columns[offset]];
@@ -81,32 +81,30 @@ int main(int argc, char *argv[])
 	}
 
 	unsigned int *d_columns;
-	check_cuda( cudaMalloc(&d_columns,   nrows * nnz * sizeof(unsigned int)) );
-	int *d_values;
-	check_cuda( cudaMalloc(&d_values ,   nrows * nnz * sizeof(         int)) );
-	int *d_x;
-	check_cuda( cudaMalloc(&d_x      ,   nrows       * sizeof(         int)) );
-	int *d_y;
-	check_cuda( cudaMalloc(&d_y      ,   nrows       * sizeof(         int)) );
+	check_cuda( cudaMalloc(&d_columns,   nrows * nnz * sizeof(unsigned int  )) );
+	float *d_values;
+	check_cuda( cudaMalloc(&d_values ,   nrows * nnz * sizeof(         float)) );
+	float *d_x;
+	check_cuda( cudaMalloc(&d_x      ,   nrows       * sizeof(         float)) );
+	float *d_y;
+	check_cuda( cudaMalloc(&d_y      ,   nrows       * sizeof(         float)) );
 
-	check_cuda( cudaMemcpy ( d_columns, columns,   nrows * nnz * sizeof(unsigned int), cudaMemcpyHostToDevice ) );
-	check_cuda( cudaMemcpy ( d_values , values ,   nrows * nnz * sizeof(         int), cudaMemcpyHostToDevice ) );
-	check_cuda( cudaMemcpy ( d_x      , x      ,   nrows       * sizeof(         int), cudaMemcpyHostToDevice ) );
+	check_cuda( cudaMemcpy ( d_columns, columns,   nrows * nnz * sizeof(unsigned int  ), cudaMemcpyHostToDevice ) );
+	check_cuda( cudaMemcpy ( d_values , values ,   nrows * nnz * sizeof(         float), cudaMemcpyHostToDevice ) );
+	check_cuda( cudaMemcpy ( d_x      , x      ,   nrows       * sizeof(         float), cudaMemcpyHostToDevice ) );
 
 	for (unsigned int i = 0; i < 5; ++i)
-		cuAlgo::gSpMatVecMulELLInt( d_columns, d_values , d_x , d_y , nrows, nnz ) ;
+		cuAlgo::gSpMatVecMulELLFloat( d_columns, d_values , d_x , d_y , nrows, nnz ) ;
 
-	check_cuda( cudaMemcpy ( y        , d_y    ,   nrows       * sizeof(         int), cudaMemcpyDeviceToHost ) );
-
-	for (unsigned int j = 0; j < nrows ; ++j) {
-		if (  solution[j] != y[j] ) {
-			std::cout << "Values are different !" << std::endl;
-		}
-	}
-
+	check_cuda( cudaFree(d_columns) );
+	check_cuda( cudaFree(d_values) );
+	check_cuda( cudaFree(d_x) );
+	check_cuda( cudaFree(d_y) );
 	free(columns);
 	free(values);
 	free(x);
 	free(y);
+	free(solution);
+
 	return 0;
 }
