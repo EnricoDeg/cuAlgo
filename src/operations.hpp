8d24f7acc38346920ec941b2e4db2ab12b7d1cd4
Enrico Degregori <degregori@dkrz.de>
2024-11-16 12:34:07 +0100

    operations: fix L2 operations

--- src/operations.hpp
+++ src/operations.hpp
@@ -82,7 +82,7 @@ class normL2_impl {
 
 	public:
 	__device__ inline T globalMemory(T * __restrict__ a, T * __restrict__ b ) {
-		return abs(*a) * abs(*a) + abs(*b) * abs(*b);
+		return (*a) * (*a) + (*b) * (*b);
 	}
 	__device__ inline void loadSharedMemory(volatile T * result, T * __restrict__ a) {
 		*result += *a;
