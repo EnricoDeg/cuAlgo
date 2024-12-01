7f963b397f98835476616188f202bded690b09b6
Enrico Degregori <degregori@dkrz.de>
2024-12-01 12:37:14 +0100

    tests: fix performance tolerance in convolution1dMatrix test

--- tests/test_convolution1dMatrix.cu
+++ tests/test_convolution1dMatrix.cu
@@ -140,7 +140,7 @@ TEST(convolution1dMatrix, performance) {
 		cuAlgo::convolution1dMatrixFloat(d_R, d_V, d_C, N, K);
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
-	ASSERT_TRUE(duration.count() / iterations < 350);
+	ASSERT_TRUE(duration.count() / iterations < 380);
 
 	check_cuda( cudaFree(d_R) );
 	check_cuda( cudaFree(d_V) );
